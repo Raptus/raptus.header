63d8f37881be00da73298ffb58e8f284c6741bd6
Simon Käser <skaeser@raptus.com>
2013-09-25 10:03:58 +0200

    Removed unused imports in interfaces.py gaining plone 4.3 compatibility

--- raptus/header/interfaces.py
+++ raptus/header/interfaces.py
@@ -1,12 +1,5 @@
-from zope import schema
 from zope.interface import Interface
 
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
-from raptus.header import headerMessageFactory as _
 
 class IHeader(Interface):
     """This header will display all contained images on top of your site"""
-    
-    # -*- schema definition goes here -*-
